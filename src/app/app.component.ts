38047b2ab308c6fc44bbd97d1e36dcb0b827e0ab
marcelnoehre <marcelnoehre@web.de>
2024-09-07 23:37:51 +0200

    raw code component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { TemplatesComponent } from './components/templates/templates.component';
+import { CodeComponent } from './components/code/code.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, TemplatesComponent],
+  imports: [RouterOutlet, ToolbarComponent, TemplatesComponent, CodeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/code/code.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code',
+  standalone: true,
+  imports: [],
+  templateUrl: './code.component.html',
+  styleUrl: './code.component.scss'
+})
+export class CodeComponent {
+
+}

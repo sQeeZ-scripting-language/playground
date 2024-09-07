4e1987301e97c7d5aedcf58d9b92812b5a7f0451
marcelnoehre <marcelnoehre@web.de>
2024-09-07 23:39:59 +0200

    raw console component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { TemplatesComponent } from './components/templates/templates.component';
 import { CodeComponent } from './components/code/code.component';
+import { ConsoleComponent } from './components/console/console.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, TemplatesComponent, CodeComponent],
+  imports: [RouterOutlet, ToolbarComponent, TemplatesComponent, CodeComponent, ConsoleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/console/console.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-console',
+  standalone: true,
+  imports: [],
+  templateUrl: './console.component.html',
+  styleUrl: './console.component.scss'
+})
+export class ConsoleComponent {
+
+}

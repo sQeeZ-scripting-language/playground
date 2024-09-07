b5cfad30cfbd504a54d15a5dd46bbefa72495bd1
marcelnoehre <marcelnoehre@web.de>
2024-09-07 23:27:44 +0200

    templates section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { TemplatesComponent } from './components/templates/templates.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, TemplatesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

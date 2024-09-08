b1afb54afac185f022d1c0866fef546414989fc7
marcelnoehre <marcelnoehre@web.de>
2024-09-08 12:10:15 +0200

    code editor

--- src/app/components/code/code.component.ts
+++ src/app/components/code/code.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-code',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [FormsModule, MatButtonModule, MatIconModule],
   templateUrl: './code.component.html',
   styleUrl: './code.component.scss'
 })
 export class CodeComponent {
+  editorContent: string = '';
+  lineNumbers: string = '1';
 
+  updateLineNumbers(event: Event) {
+    const lines = this.editorContent.split('\n').length;
+    this.lineNumbers = Array.from({ length: lines }, (_, i) => i + 1).join('\n');
+    this.syncScroll(event);
+  }
+
+  syncScroll(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+    const lineNumbers = document.querySelector('.line-numbers') as HTMLElement;
+    if (lineNumbers) lineNumbers.scrollTop = textarea.scrollTop;
+  }
 }

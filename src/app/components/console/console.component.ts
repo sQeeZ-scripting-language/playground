7adb3cf0f832b0eb7f9092823b7d16de9ed21d70
marcelnoehre <marcelnoehre@web.de>
2024-09-09 20:19:30 +0200

    add terminal design

--- src/app/components/console/console.component.ts
+++ src/app/components/console/console.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-console',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './console.component.html',
   styleUrl: './console.component.scss'
 })

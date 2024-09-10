02e262ab3afa8238c6915293b4d6dfba1df96bcf
marcelnoehre <marcelnoehre@web.de>
2024-09-10 07:19:42 +0200

    add template selection

--- src/app/components/templates/templates.component.ts
+++ src/app/components/templates/templates.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { DataService } from '../../services/data.service';
+import { TemplateCategory } from '../../interfaces/template-category.interface';
 
 @Component({
   selector: 'app-templates',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule, MatButtonModule],
   templateUrl: './templates.component.html',
-  styleUrl: './templates.component.scss'
+  styleUrl: './templates.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TemplatesComponent {
 
+  constructor(private dataService: DataService) { }
+
+  readonly panelOpenState = signal(false);
+  categories: TemplateCategory[] = [];
+
+  ngOnInit() {
+    this.categories = this.dataService.templates;
+  }
 }

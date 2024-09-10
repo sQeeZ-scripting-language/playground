27dc4b5819606292a82234a17b5e1976c242557e
marcelnoehre <marcelnoehre@web.de>
2024-09-10 17:12:40 +0200

    implement code control panel

--- src/app/components/code/code.component.ts
+++ src/app/components/code/code.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { SyncService } from '../../services/sync.service';
+import { SnackbarService } from '../../services/snackbar.service';
 
 @Component({
   selector: 'app-code',
@@ -15,7 +16,10 @@ export class CodeComponent implements OnInit {
   code: string = '';
   lineNumbers: string = '1';
 
-  constructor(private syncService: SyncService) { }
+  constructor(
+    private syncService: SyncService,
+    private snackbarService: SnackbarService
+  ) { }
 
   ngOnInit(): void {
     this.syncService.getCode().subscribe(code => {
@@ -35,4 +39,35 @@ export class CodeComponent implements OnInit {
     const lineNumbers = document.querySelector('.line-numbers') as HTMLElement;
     if (lineNumbers) lineNumbers.scrollTop = textarea.scrollTop;
   }
+
+  runCode() {
+    this.snackbarService.open('Execution of the code is not yet implemented!');
+  }
+
+  stopExecution() {
+    this.snackbarService.open('Execution of the code is not yet implemented!');
+  }
+
+  async copyCode() {
+    try {
+      await navigator.clipboard.writeText(this.code);
+      this.snackbarService.open('Code copied to clipboard!');
+    } catch (err) {
+      this.snackbarService.open('Failed to copy code to clipboard!');
+    }
+  }
+
+  clearEditor() {
+    this.syncService.setCode('');
+  }
+
+  downloadCode() {
+    const blob = new Blob([this.code], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'code.sqz';
+    a.click();
+  }
+
 }
